535be712a8006256e6008254f63ada9e91e73b42
Hasan <heshpulatov6@gmail.com>
2022-10-11 18:10:26 +0500

    homework_3

---
+++ js_homework_3/js/script.js
@@ -0,0 +1,62 @@
+// card
+
+// let a = prompt('Enter card number');
+
+// if (+a.length == 4) {
+//     console.log('Correct');
+// } else {
+//     console.log('Invalid');
+// }
+
+// days of the week
+// let weeks = prompt(`Hafta kuni raqamini kiriting`);
+
+// switch (weeks) {
+//     case '1':
+//         console.log('Dushanba');
+//         break;
+//     case '2':
+//         console.log('Seshanba');
+//         break;
+//     case '3':
+//         console.log('Chorshanba');
+//         break;
+//     case '4':
+//         console.log('Payshanba');
+//         break;
+//     case '5':
+//         console.log('Juma');
+//         break;
+//     case '6':
+//         console.log('Shanba');
+//         break;
+//     case '7':
+//         console.log('Yakshanba');
+//         break;
+
+//     default:
+//         console.log(`Bunday hafta kuni yo'q`);
+//         break;
+// }
+
+    // FIZZ BUZZ
+
+// if (num % 5 == 0 && num % 3 == 0) {
+//     console.log(`FIZZ BUZZ`);
+// } else if (num % 5 == 0) {
+//     console.log(`BUZZ`);
+// } else if (num % 3 == 0) {
+//     console.log(`FIZZ`);
+// } else {
+//     console.log(`Error`);
+// }
+
+    //odd and even numbers
+
+// let num = prompt('Raqam kiriting')
+
+// if (num % 2) {
+//     console.log('Odd number');
+// }else {
+//     console.log('Even number');
+// }
